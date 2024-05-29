4a86fcaf7a145c7cf6858a06c0a76d696ef535ad
Juliocesaru <168040425+Juliocesaru@users.noreply.github.com>
2024-05-29 19:51:13 -0400

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -44,3 +44,9 @@ os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
+ source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
