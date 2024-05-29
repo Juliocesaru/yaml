3525abbe9de3e86b8d812d1f2cbfdc15e660dceb
Juliocesaru <168040425+Juliocesaru@users.noreply.github.com>
2024-05-29 19:48:21 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,10 +6,7 @@ variable "vm_size"{
     type=string
     default="Standard_F2"
 }
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
+  
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
