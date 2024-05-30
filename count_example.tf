f311089d3ba4c604d6e003403b97c2c94e461c14
Juliocesaru <168040425+Juliocesaru@users.noreply.github.com>
2024-05-29 20:06:20 -0400

    Update count_example.tf

--- count_example.tf
+++ count_example.tf
@@ -15,7 +15,7 @@ resource "azurerm_virtual_network" "vnetexample" {
 
 resource "azurerm_subnet" "subnet" {
   name                 = "example-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name  = azurerm_resource_group.mcit420zz5um.name
   virtual_network_name = azurerm_virtual_network.vnetexample.name
   address_prefixes     = ["10.0.1.0/24"]
 }
